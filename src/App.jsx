2fc3e2f95acec7587407846f7858007ffb9a8b16
BorgesAbraao10 <abraao10clear@gmail.com>
2024-09-10 11:42:39 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+
+import styles from "./App.module.css";
+
+import "./global.css";
+import { Sidebar } from "./components/Sidebar";
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Abraão Borges"
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Voluptates nobis ullam repellendus minima fugiat vero iure qui ab velit, 
+        fugit amet vel excepturi unde a alias impedit exercitationem similique nulla?"
+          />
+
+          <Post
+            author="Ana Clara"
+            content="Um Post muito legal acompanhe minhas redes sociais"
+          />
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
