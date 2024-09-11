2628da675d4fead4ef14b5680d6a445043d954f7
BorgesAbraao10 <abraao10clear@gmail.com>
2024-09-11 09:43:55 -0300

    Last update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,20 +1,28 @@
 import styles from "./Sidebar.module.css";
 
+import { PencilLine } from "phosphor-react";
+
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
       <img
         className={styles.cover}
-        src="https://plus.unsplash.com/premium_photo-1682124651258-410b25fa9dc0?q=60&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src="https://plus.unsplash.com/premium_photo-1683121710572-7723bd2e235d?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       />
 
       <div className={styles.profile}>
-        <strong>Diego Fernandes</strong>
+        <img
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/126275703?v=4"
+        />
+        <strong>Abraão Borges</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} /> Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
