3546f4b064ad3b8b238af427e81ee79f6371e1b2
BorgesAbraao10 <abraao10clear@gmail.com>
2024-09-20 11:46:47 -0300

    Last update

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,24 +1,42 @@
-import styles from "./Post.module.css";
 import { Comment } from "./Comment";
 import { Avatar } from "./Avatar";
+import styles from "./Post.module.css";
 
-export function Post(props) {
-  console.log(props);
+export function Post({ author, publishedAt, content }) {
+  const publishedDateFormatted = new Intl.DateTimeFormat("pt-BR", {
+    day: "2-digit",
+    month: "long",
+    hour: "2-digit",
+    minute: "2-digit",
+  }).format(publishedAt);
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://avatars.githubusercontent.com/u/126275703?v=4" />
+          <Avatar src={author.avatarUrl} />
           <div>
-            <strong>Abraão Borges</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
-        <time title="11 de Setembro às 08:13h" dateTime="2022-05-11 08:13:00">
-          Publicado há 1h
+        <time title="11 de Maio as 08:13h" dateTime="2024-05-11 08:13:30">
+          {publishedDateFormatted}
         </time>
       </header>
-      <div className={styles.content}></div>
+      <div className={styles.content}>
+        {content.map((line) => {
+          if (line.type === "paragraph") {
+            return <p>{line.content}</p>;
+          } else if (line.type === "link") {
+            return (
+              <p>
+                <a href="#">{line.content}</a>
+              </p>
+            );
+          }
+        })}
+      </div>
       <form className={styles.commentForm}>
         <strong>Deixe o seu feedback</strong>
 
